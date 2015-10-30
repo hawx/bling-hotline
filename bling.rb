b336a7ce8bc9485bac5ad348d385d3362ffe81c7
David Wood <david.p.wood@gmail.com>
2015-10-30 16:13:27 +0000

    Update bling.rb

--- bling.rb
+++ bling.rb
@@ -3,6 +3,6 @@ require 'clockwork'
 api = Clockwork::API.new(ENV['CLOCKWORK_KEY'])
 
 loop do
-  api.messages.build(to: ARGV.first, content: "Bling").deliver
-  sleep 60 * 60 * 24
+  api.messages.build(to: ARGV.first, content: "$Bling$").deliver
+  sleep 24 * 60 * 60
 end
